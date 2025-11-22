ff837f809b326727a1efea348bd42f5f91b02f55
Sam <samueladeleke392@gmail.com>
2025-11-22 16:28:33 +0100

    Made the links functional

--- src/app/shared/header/header.ts
+++ src/app/shared/header/header.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-header',
@@ -6,10 +6,41 @@ import { Component } from '@angular/core';
   templateUrl: './header.html',
   styleUrl: './header.scss',
 })
-export class Header {
+export class Header implements OnInit {
   mobileMenuOpen: boolean = false;
+  activeSection: string = 'hero';
+
+  ngOnInit() {
+    const sections = document.querySelectorAll('section[id');
+
+    const options = {
+      root: null,
+      rootMargin: '0px',
+      threshold: 0.35,
+    };
+
+    const observer = new IntersectionObserver((entries) => {
+      entries.forEach((entry) => {
+        if (entry.isIntersecting) {
+          this.activeSection = entry.target.id;
+        }
+      });
+    }, options);
+
+    sections.forEach(sec => observer.observe(sec));
+  }
 
   toggleMobileMenu() {
     this.mobileMenuOpen = !this.mobileMenuOpen;
+  }
+
+  scrollTo(target: string) {
+    document.getElementById(target)?.scrollIntoView({
+      behavior: 'smooth',
+      block: 'start',
+    });
+
+    this.mobileMenuOpen = false;
+    this.activeSection = target;
   }
 }
